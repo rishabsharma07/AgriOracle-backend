f7c9ce280abf6e58f2f0f42f8b538a5eda5d2608
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-05-06 17:01:21 +0530

    update app to run on configurable port from environment variable

--- app.py
+++ app.py
@@ -180,5 +180,5 @@ def recommend_crop():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
